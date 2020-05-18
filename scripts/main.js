2ca0bac4e2f436933b815503eb458882c15a727e
AnetaStasikova <stasikovaa@gmail.com>
2020-05-18 23:57:57 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -7,13 +7,3 @@
 //neviem();
 // console.log('visible');
 //});
-
-var vol = document.getElementById('volcano2');
-
-function neviem() {
-vol.transparent = "false";
-}
-vol.addEventListener('click', function(){ 
-neviem();
- console.log('visible');
-});
