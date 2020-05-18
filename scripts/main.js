a826e1cdd43a50c8983853ebbe2364ae3f4f3c70
AnetaStasikova <stasikovaa@gmail.com>
2020-05-18 23:01:44 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,12 @@
+var volcano2 = document.getElementById('volcano2');
+ 
+ function visibility(){
+ 	
+ 	volcano2.display = "block";
+ 	console.log(volcano2.object3D.display);
+ }
 
+ volcano2.addEventListener('click', function(){ // uses a fuse
+ 	visibility();
+ 	console.log('visible');
+ });
