ca0b0bd70e51b551270273369a22feaf75a2f8ef
AnetaStasikova <stasikovaa@gmail.com>
2020-05-18 23:42:15 +0200

    .

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,9 @@
-var vol = document.getElementById('volcano2');
- 
- //function visibility(){
- 	
- 	//vol.image.display == block;
- 	//console.log(vol.display);
-// }
-//
+//var vol = document.getElementById('volcano2');
 
-
-function neviem() {
-  vol.style.display = "block";
-}
-vol.addEventListener('click', function(){ 
-neviem();
- console.log('visible');
-});
+//function neviem() {
+ // vol.style.display = "block";
+//}
+//vol.addEventListener('click', function(){ 
+//neviem();
+// console.log('visible');
+//});
