006f205e3dcbce582f471dc6373d231b94fa54fd
AnetaStasikova <stasikovaa@gmail.com>
2020-05-18 23:14:22 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,16 @@
-var vol = document.getElementById('volcano2');
+//var vol = document.getElementById('volcano2');
  
- function visibility(){
+ //function visibility(){
  	
- 	vol.display == block;
- 	console.log(vol.display);
- }
+ 	//vol.image.display == block;
+ 	//console.log(vol.display);
+// }
+//
+/// vol.addEventListener('click', function(){ 
+ 	//visibility();
+ 	//console.log('visible');
+// });
 
- vol.addEventListener('click', function(){ // uses a fuse
- 	visibility();
- 	console.log('visible');
- });
+function myFunction() {
+  document.getElementById("volcano2").style.display = "block";
+}
