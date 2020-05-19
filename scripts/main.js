e3b2a3eeaf7823c8746e74e9f9465d7ab995b639
AnetaStasikova <stasikovaa@gmail.com>
2020-05-19 21:54:15 +0200

    .

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,7 @@
 var text = document.getElementById('volcano2');
 var ball = document.getElementById('sphere');
+var track = document.getElementById('run');
+var ball2 = document.getElementById('sphere2')
 
 function visibility(){
 	text.object3D.visible = true;
@@ -15,3 +17,19 @@ ball.addEventListener('mouseleave', function(){
 	text.object3D.visible = false;
 	console.log('notvisible');
 })
+
+function visibility2(){
+	track.object3D.visible = true;
+	console.log('track.object3D.visible');
+}
+
+ball2.addEventListener('click', function(){
+	visibility2();
+	console.log('visible');
+})
+
+ball2.addEventListener('mouseleave', function(){
+	track.object3D.visible = true;
+	console.log('notvisible');
+})
+
