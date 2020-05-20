eee9a974f3d9a9cd0794fe90ae447ff86cae4adb
AnetaStasikova <stasikovaa@gmail.com>
2020-05-20 13:56:11 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -24,7 +24,7 @@ ball.addEventListener('mouseleave', function(){
 function visibility2(){
     sphere2.setAttribute('visible', false);
     track.setAttribute('visible', true); 
-    
+
 	track.setAttribute('animation__1', 'property:height;');
 	track.setAttribute('animation__1', 'loop: false;');
 
@@ -34,6 +34,7 @@ function visibility2(){
     track.setAttribute('animation__3', 'property:position;');
 	track.setAttribute('animation__3', 'loop: false;');
 	console.log('track.object3D.visible');
+	
 }
 
 ball2.addEventListener('click', function(){
