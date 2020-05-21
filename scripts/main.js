c5e9dd7fba811835508b2beb764bb33c9fb60426
AnetaStasikova <stasikovaa@gmail.com>
2020-05-21 20:35:49 +0200

    ..

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 var text = document.getElementById('volcano2');
 var ball = document.getElementById('sphere');
+var ball2 = document.getElementById('sphere2');
 var track = document.getElementById('run');
-var ball2 = document.getElementById('sphere2')
 
 function visibility(){
 	text.object3D.visible = true;
@@ -20,7 +20,26 @@ ball.addEventListener('mouseleave', function(){
 
 //* Running track
 
+function visibility2(){
 
+sphere2.setAttribute('animation__1', 'property:height;');
+track.setAttribute('animation__1', 'loop:false');
+
+sphere2.setAttribute('animation__2', 'property:width;');
+track.setAttribute('animation__2', 'loop:false');
+
+sphere2.setAttribute('animation__3', 'property:position;');
+track.setAttribute('animation__3', 'loop:false');
+
+track.setAttribute('visible', true);
+
+console.log('track.object3D.visible');
+}
+
+ball2.addEventListener('click',function(){
+	visibility2();
+	console.log('visible');
+})
 
 
 
